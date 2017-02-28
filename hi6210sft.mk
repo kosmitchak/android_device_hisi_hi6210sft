72fcd39a1d7f13f59174497215a952bd3389208c
hak86 <hakamelassouad@gmail.com>
2017-02-28 19:08:46 +0100

    hi6210sft: set lcd_density to 320

--- hi6210sft.mk
+++ hi6210sft.mk
@@ -9,3 +9,8 @@ PRODUCT_AAPT_PREF_CONFIG := xhdpi
 # Display
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    	hw.lcd.lcd_density=320 \
+    	ro.sf.lcd_density=320 \
+
