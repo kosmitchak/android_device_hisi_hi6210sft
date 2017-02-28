919c70aaac44c79af305d6001f57e62954e98761
hak86 <hakamelassouad@gmail.com>
2017-02-28 19:02:31 +0100

    hi6210sft: build aosp packages

---
+++ hi6210sft.mk
@@ -0,0 +1,3 @@
+# Android Open Source Project Common Stuff
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
