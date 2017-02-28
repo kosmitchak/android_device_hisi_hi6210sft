f4287f2bbcf93f2b90f662f44c0dd017caec567e
hak86 <hakamelassouad@gmail.com>
2017-02-28 15:55:18 +0100

    hi6210sft: initial commit

---
+++ full_hi6210sft.mk
@@ -0,0 +1,8 @@
+# Device
+$(call inherit-product, device/hisi/hi6210sft/hi6210sft.mk)
+
+PRODUCT_NAME := full_hi6210sft
+PRODUCT_DEVICE := hi6210sft
+PRODUCT_BRAND := ALE-L21
+PRODUCT_MODEL := Android 6.0 Marshmallow on Huawei P8 Lite ALE-L21 Single Sim
+PRODUCT_MANUFACTURER := HUAWEI
