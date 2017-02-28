872ee7de1f94c9e6a896d580d952aa7f11139421
hak86 <hakamelassouad@gmail.com>
2017-02-28 19:05:45 +0100

    hi6210sft: set PRODUCT_AAPT_CONFIG

--- hi6210sft.mk
+++ hi6210sft.mk
@@ -1,3 +1,7 @@
 # Android Open Source Project Common Stuff
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# AAPT
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
