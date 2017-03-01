59b80a304a1c2babb9401fbbbeef76d6656091b8
hak86 <hakamelassouad@gmail.com>
2017-03-01 12:45:43 +0100

    hi6210sft: switch model to ALE-L21

--- full_hi6210sft.mk
+++ full_hi6210sft.mk
@@ -4,5 +4,5 @@ $(call inherit-product, device/hisi/hi6210sft/hi6210sft.mk)
 PRODUCT_NAME := full_hi6210sft
 PRODUCT_DEVICE := hi6210sft
 PRODUCT_BRAND := ALE-L21
-PRODUCT_MODEL := Android 6.0 Marshmallow on Huawei P8 Lite ALE-L21 Single Sim
+PRODUCT_MODEL := ALE-L21
 PRODUCT_MANUFACTURER := HUAWEI
--- omni_hi6210sft.mk
+++ omni_hi6210sft.mk
@@ -4,7 +4,7 @@ $(call inherit-product, device/hisi/hi6210sft/full_hi6210sft.mk)
 PRODUCT_NAME := omni_hi6210sft
 PRODUCT_DEVICE := hi6210sft
 PRODUCT_BRAND := ALE-L21
-PRODUCT_MODEL := OmniROM 6.0 Marshmallow on Huawei P8 Lite ALE-L21 Single Sim
+PRODUCT_MODEL := ALE-L21
 PRODUCT_MANUFACTURER := HUAWEI
 
 # OmniROM 6.0 Marshmallow
