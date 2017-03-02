79ce71a0d82d371e0d62baf4d4af18c436cd0f8a
hak86 <hakamelassouad@gmail.com>
2017-03-02 17:08:14 +0100

    hi6210sft: update aapt configs

--- hi6210sft.mk
+++ hi6210sft.mk
@@ -6,7 +6,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # AAPT
-PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_CONFIG := xhdpi hdpi normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
 # Dalvik
