0485c98b010738216d2e725f0e9ccfcbb22214a1
hak86 <hakamelassouad@gmail.com>
2017-03-03 01:13:50 +0100

    hi6210sft: Audio: copy codecs

--- hi6210sft.mk
+++ hi6210sft.mk
@@ -30,6 +30,15 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/rootdir/system/etc/audio_effects.conf:system/etc/audio_effects.conf \
 	$(LOCAL_PATH)/rootdir/system/etc/audio_policy.conf:system/etc/audio_policy.conf \
 
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/rootdir/system/etc/hisi_omx.cfg:system/etc/hisi_omx.cfg \
+	$(LOCAL_PATH)/rootdir/system/etc/media_codecs.xml:system/etc/media_codecs.xml \
+	$(LOCAL_PATH)/rootdir/system/etc/media_codecs_ffmpeg.xml:system/etc/media_codecs_ffmpeg.xml\
+	$(LOCAL_PATH)/rootdir/system/etc/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+	$(LOCAL_PATH)/rootdir/system/etc/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+	$(LOCAL_PATH)/rootdir/system/etc/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
+	$(LOCAL_PATH)/rootdir/system/etc/media_profiles.xml:system/etc/media_profiles.xml \
+
 # Binaries
 PRODUCT_COPY_FILES += \
 	$(call find-copy-subdir-files,*,$(LOCAL_PATH)/rootdir/system/bin,system/bin) \
