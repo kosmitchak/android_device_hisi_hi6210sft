15b10487896aa579ef4020872934b73a13c4a1eb
hak86 <hakamelassouad@gmail.com>
2017-11-25 12:14:19 +0100

    hi6210sft: update zygote64_32 to fix boot

--- hi6210sft.mk
+++ hi6210sft.mk
@@ -136,3 +136,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Zygote
 ADDITIONAL_DEFAULT_PROPERTIES += \
 	ro.zygote=zygote64_32
+
+PRODUCT_COPY_FILES += \
+	system/core/rootdir/init.zygote64_32.rc:root/init.zygote64_32.rc
+
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+	ro.zygote=zygote64_32
+
