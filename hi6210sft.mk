6d04d96c3afe807e0314c7530c674faccd6246a6
hak86 <hakamelassouad@gmail.com>
2017-11-25 12:05:47 +0100

    hi6210sft: copy blobs from one command

--- hi6210sft.mk
+++ hi6210sft.mk
@@ -23,67 +23,14 @@ PRODUCT_PACKAGES += \
      	tinypcminfo \
      	tinyplay \
 
-PRODUCT_COPY_FILES += \
-	$(call find-copy-subdir-files,*,$(LOCAL_PATH)/rootdir/system/etc/audio,system/etc/audio) \
-	$(call find-copy-subdir-files,*,$(LOCAL_PATH)/rootdir/system/etc/srs,system/etc/srs) \
-	$(call find-copy-subdir-files,*,$(LOCAL_PATH)/rootdir/system/etc/sws,system/etc/sws) \
-	$(LOCAL_PATH)/rootdir/system/etc/audio_effects.conf:system/etc/audio_effects.conf \
-	$(LOCAL_PATH)/rootdir/system/etc/audio_policy.conf:system/etc/audio_policy.conf \
-
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/rootdir/system/etc/hisi_omx.cfg:system/etc/hisi_omx.cfg \
-	$(LOCAL_PATH)/rootdir/system/etc/media_codecs.xml:system/etc/media_codecs.xml \
-	$(LOCAL_PATH)/rootdir/system/etc/media_codecs_ffmpeg.xml:system/etc/media_codecs_ffmpeg.xml\
-	$(LOCAL_PATH)/rootdir/system/etc/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
-	$(LOCAL_PATH)/rootdir/system/etc/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
-	$(LOCAL_PATH)/rootdir/system/etc/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
-	$(LOCAL_PATH)/rootdir/system/etc/media_profiles.xml:system/etc/media_profiles.xml \
-
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/rootdir/system/lib/hw/audio.primary.hi6210sft.so:system/lib/hw/audio.primary.hi6210sft.so \
-	$(LOCAL_PATH)/rootdir/system/lib/hw/audio.r_submix.default.so:system/lib/hw/audio.r_submix.default.so \
-	$(LOCAL_PATH)/rootdir/system/lib/hw/hw_af_extend.default.so:system/lib/hw/hw_af_extend.default.so \
-	$(LOCAL_PATH)/rootdir/system/lib/libaudioalgoservice_jni.so:system/lib/libaudioalgoservice_jni.so \
-	$(LOCAL_PATH)/rootdir/system/lib/libaudio_custom.so:system/lib/libaudio_custom.so \
-	$(LOCAL_PATH)/rootdir/system/lib/libaudioflinger.huawei.so:system/lib/libaudioflinger.hauwei.so \
-	$(LOCAL_PATH)/rootdir/system/lib/libaudioroute.so:system/lib/libaudioroute.so \
-	$(LOCAL_PATH)/rootdir/system/lib/libbalong_audio_ril.so:system/lib/libbalong_audio_ril.so \
-	$(LOCAL_PATH)/rootdir/system/lib/libearpa.so:system/lib/libearpa.so \
-	$(LOCAL_PATH)/rootdir/system/lib/libmax98925.so:system/lib/libmax98925.so \
-	$(LOCAL_PATH)/rootdir/system/lib/libnvme.so:system/lib/libnvme.so \
-	$(LOCAL_PATH)/rootdir/system/lib/libOpenMAXAL.so:system/lib/libOpenMAXAL.so \
-	$(LOCAL_PATH)/rootdir/system/lib/libOpenSLES.so:system/lib/libOpenSLES.so \
-	$(LOCAL_PATH)/rootdir/system/lib/libsoundtrigger.so:system/lib/libsoundtrigger.so \
-	$(LOCAL_PATH)/rootdir/system/lib/libsoundtriggerservice.so:system/lib/libsoundtriggerservice.so \
-	$(LOCAL_PATH)/rootdir/system/lib/libsrsprocessing.so:system/lib/libsrsprocessing.so \
-	$(LOCAL_PATH)/rootdir/system/lib/libswsprocessing.so:system/lib/libswsprocessing.so \
-	$(LOCAL_PATH)/rootdir/system/lib/libtfa9895.so:system/lib/libtfa9895.so \
-
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/rootdir/system/lib64/hw/audio.primary.hi6210sft.so:system/lib64/hw/audio.primary.hi6210sft.so \
-	$(LOCAL_PATH)/rootdir/system/lib64/hw/audio.r_submix.default.so:system/lib64/hw/audio.r_submix.default.so \
-	$(LOCAL_PATH)/rootdir/system/lib64/libbalong_audio_ril.so:system/lib64/libbalong_audio_ril.so \
-	$(LOCAL_PATH)/rootdir/system/lib64/libearpa.so:system/lib64/libearpa.so \
-	$(LOCAL_PATH)/rootdir/system/lib64/libmax98925.so:system/lib64/libmax98925.so \
-	$(LOCAL_PATH)/rootdir/system/lib64/libnvme.so:system/lib64/libnvme.so \
-	$(LOCAL_PATH)/rootdir/system/lib64/libOpenMAXAL.so:system/lib64/libOpenMAXAL.so \
-	$(LOCAL_PATH)/rootdir/system/lib64/libOpenSLES.so:system/lib64/libOpenSLES.so \
-	$(LOCAL_PATH)/rootdir/system/lib64/libsoundtrigger.so:system/lib64/libsoundtrigger.so \
-	$(LOCAL_PATH)/rootdir/system/lib64/libsoundtriggerservice.so:system/lib64/libsoundtriggerservice.so \
-	$(LOCAL_PATH)/rootdir/system/lib64/libswsprocessing.so:system/lib64/libswsprocessing.so \
-	$(LOCAL_PATH)/rootdir/system/lib64/libtfa9895.so:system/lib64/libtfa9895.so \
-
 # Binaries
 PRODUCT_COPY_FILES += \
-	$(call find-copy-subdir-files,*,$(LOCAL_PATH)/rootdir/system/bin,system/bin) \
+	$(call find-copy-subdir-files,*,$(LOCAL_PATH)/rootdir/system/,system/) \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
     	bt_vendor.conf \
     	libbt-vendor \
-
-PRODUCT_COPY_FILES += \
-	$(call find-copy-subdir-files,*,$(LOCAL_PATH)/rootdir/system/etc/bluetooth,system/etc/bluetooth) \
 
 # Dalvik
 PRODUCT_TAGS += dalvik.gc.type-precise
@@ -98,47 +45,8 @@ PRODUCT_PACKAGES += \
     	make_ext4fs \
     	setup_fs
 
-# Firmware
-PRODUCT_COPY_FILES += \
-	$(call find-copy-subdir-files,*,$(LOCAL_PATH)/rootdir/system/vendor/firmware,system/vendor/firmware) \
-
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/rootdir/system/lib/libbt_factory_test.so:system/lib/libbt_factory_test.so \
-	$(LOCAL_PATH)/rootdir/system/lib/libbt_factory_test_hi110x.so:system/lib/libbt_factory_test_hi110x.so \
-
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/rootdir/system/lib64/libbt_factory_test.so:system/lib64/libbt_factory_test.so \
-	$(LOCAL_PATH)/rootdir/system/lib64/libbt_factory_test_hi110x.so:system/lib64/libbt_factory_test_hi110x.so \
-
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/rootdir/system/vendor/lib/libbt-vendor.so:system/vendor/lib/libbt-vendor.so \
-	$(LOCAL_PATH)/rootdir/system/vendor/lib/libbt-vendor-hi110x.so:system/vendor/lib/libbt-vendor-hi110x.so \
-
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/rootdir/system/vendor/lib64/libbt-vendor-hi110x.so:system/vendor/lib64/libbt-vendor-hi110x.so \
-
 # GPS
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
-
-PRODUCT_COPY_FILES += \
-	$(call find-copy-subdir-files,*,$(LOCAL_PATH)/rootdir/system/etc/gnss,system/etc/gnss) \
-	$(LOCAL_PATH)/rootdir/system/etc/gps.conf:system/etc/gps.conf \
-	$(LOCAL_PATH)/rootdir/system/etc/gpsconfig.xml:system/etc/gpsconfig.xml \
-	$(LOCAL_PATH)/rootdir/system/etc/gpsconfig_beta.xml:system/etc/gpsconfig_beta.xml \
-	$(LOCAL_PATH)/rootdir/system/etc/hisi_cfg.ini:system/etc/hisi_cfg.ini \
-	$(LOCAL_PATH)/rootdir/system/etc/hisi_cfg_alice.ini:system/etc/hisi_cfg_alice.ini \
-	$(LOCAL_PATH)/rootdir/system/etc/hisi_cfg_cherry.ini:system/etc/hisi_cfg_cherry.ini \
-
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/rootdir/system/lib/hw/gps.hi110x.default.so:system/lib/hw/gps.hi110x.default.so \
-	$(LOCAL_PATH)/rootdir/system/lib/libgps_factory_test.so:system/lib/libgps_factory_test.so \
-	$(LOCAL_PATH)/rootdir/system/lib/libgps_factory_test_hi110x.so:system/lib/libgps_factory_test_hi110x.so \
-
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/rootdir/system/lib64/hw/libgps_factory_test.so:system/lib64/hw/libgps_factory_test.so \
-	$(LOCAL_PATH)/rootdir/system/lib64/hw/libgps_factory_test_hi110x.so:system/lib64/hw/libgps_factory_test_hi110x.so \
-	$(LOCAL_PATH)/rootdir/system/lib64/libgps_factory_test.so:system/lib64/libgps_factory_test.so \
-	$(LOCAL_PATH)/rootdir/system/lib64/libgps_factory_test_hi110x.so:system/lib64/libgps_factory_test_hi110x.so \
 
 # Graphics
 PRODUCT_PACKAGES += \
@@ -152,18 +60,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     	persist.sys.strictmode.disable=1 \
     	persist.sys.use_dithering=2 \
     	ro.opengles.version=131072 \
-
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/rootdir/system/lib/egl/libGLES_android.so:system/lib/egl/libGLES_android.so \
-	$(LOCAL_PATH)/rootdir/system/lib/egl/libGLES_mali.so:system/lib/egl/libGLES_mali.so \
-	$(LOCAL_PATH)/rootdir/system/lib/hw/gralloc.hi6210sft.so:system/lib/hw/gralloc.hi6210sft.so \
-	$(LOCAL_PATH)/rootdir/system/lib/libion.so:system/lib/libion.so \
-
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/rootdir/system/lib64/egl/libGLES_android.so:system/lib64/egl/libGLES_android.so \
-	$(LOCAL_PATH)/rootdir/system/lib64/egl/libGLES_mali.so:system/lib64/egl/libGLES_mali.so \
-	$(LOCAL_PATH)/rootdir/system/lib64/hw/gralloc.hi6210sft.so:system/lib64/hw/gralloc.hi6210sft.so \
-	$(LOCAL_PATH)/rootdir/system/lib64/libion.so:system/lib64/libion.so \
 
 # Huawei P8 Lite Components
 PRODUCT_PACKAGES += \
@@ -180,19 +76,7 @@ PRODUCT_PACKAGES += \
     	libnfc_nci_jni \
     	nfc_nci.pn54x.default \
     	NfcNci \
-    	Tag 
-
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/rootdir/system/etc/libnfc-brcm-alice.conf:system/etc/libnfc-brcm.conf \
-	$(LOCAL_PATH)/rootdir/system/etc/libnfc-nxp-alice.conf:system/etc/libnfc-nxp.conf \
-	$(LOCAL_PATH)/rootdir/system/etc/nfcee_access.xml:system/etc/nfcee_access.xml \
-
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/rootdir/system/lib64/hw/nfc_nci.pn54x.default.so:system/lib64/hw/nfc_nci.pn54x.default.so
-
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/rootdir/system/vendor/firmware/bcm2079x_firmware.ncd:system/vendor/firmware/bcm2079x_firmware.ncd \
-	$(LOCAL_PATH)/rootdir/system/vendor/firmware/bcm2079x_pre_firmware.ncd:system/vendor/firmware/bcm2079x_pre_firmware.ncd \
+    	Tag
 
 PRODUCT_COPY_FILES += \
     	frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
@@ -239,22 +123,6 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/rootdir/init.hi6210sft.rc:root/init.hi6210sft.rc \
 	$(LOCAL_PATH)/rootdir/init.recovery.hi6210sft.rc:root/init.recovery.hi6210sft.rc \
 	$(LOCAL_PATH)/rootdir/ueventd.hi6210sft.rc:root/ueventd.hi6210sft.rc \
-
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/rootdir/system/lib/hw/sensorcaps.default.so:system/lib/hw/sensorcaps.default.so \
-	$(LOCAL_PATH)/rootdir/system/lib/hw/sensors.default.so:system/lib/hw/sensors.default.so \
-
-# Sensors
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/rootdir/system/lib64/hw/lights.default.so:system/lib64/hw/lights.hi6210sft.so \
-	$(LOCAL_PATH)/rootdir/system/lib64/hw/sensorcaps.default.so:system/lib64/hw/sensorcaps.default.so \
-	$(LOCAL_PATH)/rootdir/system/lib64/hw/sensors.default.so:system/lib64/hw/sensors.default.so \
-
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/rootdir/system/vendor/lib/libcalmodule_akm.so:system/vendor/lib/libcalmodule_akm.so \
-
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/rootdir/system/vendor/lib64/libcalmodule_akm.so:system/vendor/lib64/libcalmodule_akm.so \
 
 # Zygote
 ADDITIONAL_DEFAULT_PROPERTIES += \
